23c19d24474c128f79266730180cb640befb52bd
shanks05-SPEC <shashank.21cs093@sode.edu.in>
2025-07-18 14:58:49 +0530

    chat app

---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
+    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden":""}`}>
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt=""
+        className='w-20 aspect-[1/1] rounded-full'/>
+        <h1 className='px-10 text-x1 font-medium mx-auto flex items-center gap-2'>
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}
+        </h1>
+        <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+      <hr className="border-[#ffffff50] my-4"/>
+      <div className="px-5 text-xs">
+        <p>Media</p>
+        <div className=''
+      </div>
+    </div>
+  )
+}
+
+export default RightSidebar
